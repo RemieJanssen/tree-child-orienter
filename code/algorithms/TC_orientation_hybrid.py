7a48cabe890f031f36bf9bc16b6fbee3b5791646
Remie Janssen <jansser@rivm-biohn-l06p.int.ssc-campus.nl>
2025-02-12 18:26:50 +0100

    more efficient hybrid

--- code/algorithms/TC_orientation_hybrid.py
+++ code/algorithms/TC_orientation_hybrid.py
@@ -6,11 +6,8 @@ from phyloroot import LevelStuff, ClassAllNetworks, OrientationAlgorithmBinary
 from unittest import mock
 
 
-def NewRootAtEdge(network,rootEdge,ClassChecker=ClassAllNetworks):
+def NewRootAtEdge(network,rootEdge,min_cycle,ClassChecker=ClassAllNetworks):
     noOfReticulations = len(network.edges)-len(network.nodes)+1
-
-    # Array for storing minimal cycles
-    min_cycle = nx.minimum_cycle_basis(network)
 
     # Array to store one selection of vertices from each minimal cycle
     r_set = list(itertools.product(*min_cycle))
@@ -24,10 +21,20 @@ def NewRootAtEdge(network,rootEdge,ClassChecker=ClassAllNetworks):
             return reticulations
     return False
 
+def NewClassRootableStupid(network,ClassChecker=ClassAllNetworks):
+    rootings = dict()
+    # Array for storing minimal cycles
+    min_cycle = nx.minimum_cycle_basis(network)
+    for e in network.edges:
+        result = NewRootAtEdge(network,e,min_cycle,ClassChecker)
+        if result:
+            rootings[e]=result
+    return rootings
+
 def tree_child_orient_hybrid(G):
   # Run orientation algorithm Huber2024 FPT
   ClassChecker, length = get_class_checker_and_chain_length("TC")
-  with mock.patch("phyloroot.rooting.RootAtEdge", NewRootAtEdge):
+  with mock.patch("phyloroot.rooting.ClassRootableStupid", NewClassRootableStupid):
     orientations = LevelStuff(G, length, ClassChecker)
 
   if orientations:
